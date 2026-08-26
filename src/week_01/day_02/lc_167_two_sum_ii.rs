b9fbb022bbfee23eb315523fb942bcad37162aae
a.izadi <izadi.ali.me@gmail.com>
2026-08-26 13:27:55 +0330

    solve lc_167_two_sum_ii.rs

--- src/week_01/day_02/lc_167_two_sum_ii.rs
+++ src/week_01/day_02/lc_167_two_sum_ii.rs
@@ -2,8 +2,26 @@
 
 #![allow(unused_variables)]
 
-pub fn solve(nums: &[i32], target: i32) -> Option<(usize, usize)> {
-    todo!("implement opposite-end two pointers")
+pub fn solve(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+    if numbers.len() < 2 {
+        return vec![];
+    }
+
+    let mut start_index = 0;
+    let mut end_index = numbers.len() - 1;
+
+    while start_index != end_index {
+        let sum = numbers[start_index] + numbers[end_index];
+        if sum == target {
+            return vec![start_index as i32, end_index as i32];
+        }
+        if sum > target {
+            end_index -= 1;
+        } else if sum < target {
+            start_index += 1;
+        }
+    }
+    vec![]
 }
 
 #[cfg(test)]
@@ -11,27 +29,24 @@ mod tests {
     use super::solve;
 
     #[test]
-    #[ignore = "Exercise scaffold: enable after implementing solve"]
     fn finds_pairs_at_edges_and_with_duplicate_values() {
-        assert_eq!(solve(&[2, 7, 11, 15], 9), Some((0, 1)));
-        assert_eq!(solve(&[-5, -1, 0, 4, 10], 5), Some((0, 4)));
-        assert_eq!(solve(&[1, 1, 3], 2), Some((0, 1)));
+        assert_eq!(solve(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(solve(vec![-5, -1, 0, 4, 10], 5), vec![0, 4]);
+        assert_eq!(solve(vec![1, 1, 3], 2), vec![0, 1]);
     }
 
     #[test]
-    #[ignore = "Exercise scaffold: enable after implementing solve"]
     fn reports_no_pair_for_short_or_impossible_inputs() {
-        assert_eq!(solve(&[], 0), None);
-        assert_eq!(solve(&[4], 8), None);
-        assert_eq!(solve(&[1, 3, 5], 7), None);
+        assert_eq!(solve(vec![], 0), vec![]);
+        assert_eq!(solve(vec![4], 8), vec![]);
+        assert_eq!(solve(vec![1, 3, 5], 7), vec![]);
     }
 
     #[test]
-    #[ignore = "Exercise scaffold: enable after implementing solve"]
     fn handles_negative_targets_zero_and_pairs_away_from_the_edges() {
-        assert_eq!(solve(&[-8, -5, -2, 1, 4], -10), Some((0, 2)));
-        assert_eq!(solve(&[-4, -1, 0, 3, 8], 4), Some((0, 4)));
-        assert_eq!(solve(&[1, 2, 4, 6, 10], 8), Some((1, 3)));
-        assert_eq!(solve(&[0, 0], 0), Some((0, 1)));
+        assert_eq!(solve(vec![-8, -5, -2, 1, 4], -10), vec![0, 2]);
+        assert_eq!(solve(vec![-4, -1, 0, 3, 8], 4), vec![0, 4]);
+        assert_eq!(solve(vec![1, 2, 4, 6, 10], 8), vec![1, 3]);
+        assert_eq!(solve(vec![0, 0], 0), vec![0, 1]);
     }
 }
